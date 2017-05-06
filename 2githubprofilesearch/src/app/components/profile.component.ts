dc8c2f74395548f0a7629d69d639f4ceca3c02d1
kirandash <thekirankumardash@gmail.com>
2017-05-06 23:49:03 +0530

    2 Search Feature with FormModules

--- 2githubprofilesearch/src/app/components/profile.component.ts
+++ 2githubprofilesearch/src/app/components/profile.component.ts
@@ -8,17 +8,32 @@ import { GithubService } from '../services/github.services';
 })
 export class ProfileComponent  { 
 	// Property for user
-	user:any[];
+	user:any;
 	repos:any[];
+	username:string;
 	
 	constructor(private _githubService: GithubService){
 		// Subscribe to GithubService
+		// Now this will be called when a search is performed
+		/*this._githubService.getUser().subscribe(user => {
+			this.user = user;
+		});
+		// Subscribe to GithubService
+		this._githubService.getRepos().subscribe(repos => {
+			this.repos = repos;
+		});*/
+		this.user = false; // default user state
+	}
+
+	searchUser() {
+		this._githubService.updateUser(this.username);
+
 		this._githubService.getUser().subscribe(user => {
 			this.user = user;
 		});
 		// Subscribe to GithubService
 		this._githubService.getRepos().subscribe(repos => {
 			this.repos = repos;
-		});		
+		});	
 	}
 }
--- 2githubprofilesearch/src/app/services/github.services.ts
+++ 2githubprofilesearch/src/app/services/github.services.ts
@@ -25,4 +25,8 @@ export class GithubService{
 		return this._http.get('http://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret).map(res => res.json());
 		// map is used to convert received data to JSON
 	}
+
+	updateUser(username:string){
+		this.username = username;
+	}
 }
