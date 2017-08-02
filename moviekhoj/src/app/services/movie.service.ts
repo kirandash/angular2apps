6ba51df431e6cde201f2afc81c2aada16ee9a260
kirandash <thekirankumardash@gmail.com>
2017-08-02 23:33:59 +0530

    MK - Create getPopular and getInTheatres service

--- moviekhoj/src/app/services/movie.service.ts
+++ moviekhoj/src/app/services/movie.service.ts
@@ -7,8 +7,18 @@ import 'rxjs/Rx'; //import reactive extension
 @Injectable()
 export class MovieService {
   apikey: string;
+  
   constructor(private _jsonp: Jsonp) { 
   	this.apikey = '5a7f3263e577b104f411553bf574446b';
   	console.log('This service is initialized...');
+  }
+  
+  getPopular() {
+  	// Callback and API keys are important
+  	return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc&api_key='+this.apikey).map(res => res.json()); // map is used since it is going to be an observable
+  }
+
+  getIntheatres() {
+  	return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2017-07-01&primary_release_date.lte=2017-08-30&api_key='+this.apikey).map(res => res.json());
   }
 }
