d7cdbd31ac704783471ac38167fa658311cb1407
kirandash <thekirankumardash@gmail.com>
2017-08-03 13:56:29 +0530

    AuthApp project setup

---
+++ authapp/README.md
@@ -0,0 +1,9 @@
+# Authapp
+
+https://auth0.com
+
+Dashboard - new client
+
+npm install angular2-jwt --save (Helper library for auth0)
+
+npm install auth0-lock --save (lock widget for login box)
---
+++ authapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
