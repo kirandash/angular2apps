3c60f90fb494b80d3fef503422f78e67b13e23fe
kirandash <thekirankumardash@gmail.com>
2017-08-03 18:58:21 +0530

    Add components

--- angular4/README.md
+++ angular4/README.md
@@ -3,3 +3,19 @@
 ng new angular4 --prefix ag
 
 ng serve
+
+ng g component movies-list
+
+ng g c characters-list
+
+When angular runs first app-root is bootstrapped from index.html file which then goes to main.js file and the bootstrapModule function is called with AppModule parameter which then takes us to app.module.ts file.
+
+# Components
+Components are significant building blocks of angular architecture. Applications are composed with components.
+
+Component holds the data + logic and provide visual structure.
+
+Parent - Child hierarchy
+
+# Routing
+
--- angular4/src/app/app.module.ts
+++ angular4/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent, // Components declared here are added to the root level context of the app which means it can be accessed by any other component
+    CharactersListComponent
   ],
   imports: [
     BrowserModule,
