e631f0e8f1f6cdfc4f5e71d864bc9ed4f6deb26a
kirandash <thekirankumardash@gmail.com>
2017-08-03 20:06:14 +0530

    Angular4 Access services

--- angular4/src/app/components/characters-list/characters-list.component.ts
+++ angular4/src/app/components/characters-list/characters-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+// Accessing services by adding classes
+import { Character } from '../../classes/character';
+import { CharactersService } from '../../services/characters.service';
 
 @Component({
   selector: 'app-characters-list',
   templateUrl: './characters-list.component.html',
-  styleUrls: ['./characters-list.component.css']
+  styleUrls: ['./characters-list.component.css'],
+  providers: [CharactersService] // add class to provider MoviesService
 })
 export class CharactersListComponent implements OnInit {
 
-  constructor() { }
+  // define variable
+  characters: Character[];
 
+  // Inject CharactersService
+  constructor(private charactersService: CharactersService) {
+
+  }
+
+  getCharacters(): void { // returns void. nothing else
+  	this.charactersService.getCharacters().then(characters => this.characters = characters); // then fn used to retrieve data from promise.
+  }
+
+  // getCharacters fn is called when class is instantiated
   ngOnInit() {
+  	this.getCharacters();
   }
 
 }
--- angular4/src/app/components/movies-list/movies-list.component.ts
+++ angular4/src/app/components/movies-list/movies-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+// Accessing services by adding classes
+import { Movie } from '../../classes/movie';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.css']
+  styleUrls: ['./movies-list.component.css'],
+  providers: [MoviesService] // add class to provider MoviesService
 })
 export class MoviesListComponent implements OnInit {
+  
+  // define variable
+  movies: Movie[];
 
-  constructor() { }
+  // Inject MoviesService
+  constructor(private moviesService: MoviesService) {
 
+  }
+
+  getMovies(): void { // returns void. nothing else
+  	this.moviesService.getMovies().then(movies => this.movies = movies); // then fn used to retrieve data from promise.
+  }
+
+  // getMovies fn is called when class is instantiated
   ngOnInit() {
+  	this.getMovies();
   }
 
 }
