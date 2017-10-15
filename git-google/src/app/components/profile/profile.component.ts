4c46ec1494ef53d6771ae985300a72c4f24dfbfc
kirandash <thekirankumardash@gmail.com>
2017-10-15 15:10:41 +0530

    GitGoogle - 4 Display user repos

--- git-google/src/app/components/profile/profile.component.ts
+++ git-google/src/app/components/profile/profile.component.ts
@@ -7,15 +7,20 @@ import { ProfileService } from '../../services/profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  // Define properties
   profile:any[];
-
+  repos:any[];
 
   constructor(private profileService: ProfileService) { 
   	this.profileService.getProfileInfo().subscribe( profile => {
   		console.log(profile); // return all the infor for the username
       this.profile = profile;
   	} ); // subscribe to the observable being returned
+
+    this.profileService.getProfileRepos().subscribe( repos => {
+      console.log(repos); // return all the infor for the username
+      this.repos = repos;
+    } ); // subscribe to the observable being returned
   }
 
   ngOnInit() {
--- git-google/src/app/services/profile.service.ts
+++ git-google/src/app/services/profile.service.ts
@@ -21,4 +21,9 @@ export class ProfileService {
   		.map(res => res.json()); // observable is returned
   }
 
+  getProfileRepos(){
+    return this.http.get("http://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+      .map(res => res.json()); // observable is returned
+  }
+
 }
